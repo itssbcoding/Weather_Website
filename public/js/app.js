d09ad31bfe8aaaa5452b07e048155b944d50bf3e
Shubham SB <shubham.sb.48@gmail.com>
2020-06-29 01:53:09 +0530

    Unnecessary Console.log Removed

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client Side javascript file is loaded.')
-
 const weatherForm=document.querySelector('form')
 const search=document.querySelector('input')
 const messageOne=document.querySelector('#message-1')//for chosing a html attribute by id, we use #
